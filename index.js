9d3c58fb70aca2570f42a70e57be74a56b055c10
amorim.pedro@engenharia.ufjf.br <amorim.pedro@engenharia.ufjf.br>
2022-05-10 10:47:45 -0300

    Fix bugs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import express, { json } from "express";
 import joi from "joi";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient, ObjectId } from "mongodb";
-import { v4 as uuid } from "uuid";
-import dataBase from "dataBase.js";
+import { Db, MongoClient, ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+// import db from "./db.js";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+const promise = mongoClient.connect();
+promise.catch((e) => console.log("Erro na conexão ao banco de dados", e));
+const db = mongoClient.db(process.env.BANCO);
 
 const app = express();
 app.use(json());
 app.use(cors());
-dotenv.config();
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -25,16 +29,19 @@ app.post("/login", async (req, res) => {
     return res.status(422).send(error.details.map((detail) => detail.message));
   }
   try {
-    const user = await dataBase.collection("users").findOne({ email });
+    const user = await db.collection("users").findOne({ email });
+    console.log(user);
     if (user && bcrypt.compareSync(password, user.password)) {
-      const token = uuid.v4();
-      await dataBase.collection("sessions").insertOne({ userId: user._id, token });
-      res.send(token);
+      console.log("entrou");
+      const token = uuid();
+      await db.collection("sessions").insertOne({ userId: user._id, token });
+      res.status(200).send(token);
     } else {
       res.status(401).send("Email/Senha incorretas!");
     }
   } catch (error) {
-    res.send("A tentativa de logar apresentou o seguinte erro:", e);
+    console.log("Erro ao logar", error);
+    res.sendStatus(500);
   }
 });
 
@@ -53,16 +60,18 @@ app.post("/sign-up", async (req, res) => {
     return res.status(422).send(error.details.map((detail) => detail.message));
   }
   try {
-    const verificador = dataBase.collection("users").findOne({ email });
-    if (verificador) {
+    const find = await db.collection("users").findOne({ email });
+    console.log(find);
+    if (find) {
       return res.status(409).send("Já existe um usuário com esse e-mail.");
     }
     const passwordHash = bcrypt.hashSync(password, 10);
-    await dataBase.collection("users").insertOne({ email, name, passwordHash });
-    res.sendStatus(201);
+    await db
+      .collection("users")
+      .insertOne({ email, name, password: passwordHash });
+    res.status(201).send();
   } catch (error) {
-    res.send("A tentativa de logar apresentou o seguinte erro:", e);
+    console.log("Erro ao cadastrar", error);
+    res.sendStatus(500);
   }
 });
-
-app.listen(process.env.PORTA);
